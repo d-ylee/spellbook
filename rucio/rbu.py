57aa32455683a06db417a2d01f3670dda7d67a56
Brandon White <bjwhite@fnal.gov>
2022-03-03 14:39:21 -0700

    Complete first draft of rbu.py? Needs testing, but first I'm going climbing.

--- rucio/rbu.py
+++ rucio/rbu.py
@@ -14,48 +14,39 @@ logger = logging.getLogger('rbu')
 
 
 class RucioUploader:
-    def __init__(self, args):
-        self.dataset_name = args.dataset_name
-        self.rucio_client = RucioClient(account=self.args.rucio_account)
+    def __init__(self, rucio_account, account, scope, dataset_name, rse):
+        self.rucio_account = rucio_account
+        self.scope = scope
+        self.dataset_name = dataset_name
+        self.rse = rse 
+        self.rucio_client = RucioClient(account=self.rucio_account)
         self.rucio_upload_client = RucioUploadClient(_client=self.rucio_client, logger=logger)
 
+    def do_processing(self, tid, files):
+        upload_items = prepare_items(files)
+        self.rucio_upload_client.upload(items)
 
-    def do_processing(self, tid, files, args):
+    def prepare_items(self, files):
+        items = []
         for f in files:
-            #print(f'(tid {tid})f: {f}')
-            # TODO: Upload the file
-            rucio_upload(f)
-
+            item = {
+                'path': f,
+                'rse': self.start_rse,
+                'did_scope': self.rucio_scope,
+                'dataset_scope': self.rucio_scope,
+                'dataset_name': self.dataset_name,
+            }
+            items.append(item)
+        return items
 
     def rucio_create_dataset(self):
-        account_arg = '-a {rucio_account}'.format(rucio_account=self.rucio_account)
-        dataset_did = 'user.{rucio_account}:{dataset_name}'.format(rucio_account=self.rucio_account, dataset_name=dataset_name)
-        cmd = 'rucio {account_arg} add-dataset {dataset_did}'\
-            .format(account_arg=account_arg, dataset_did=dataset_did)
-        logger.info(f'Running command: {cmd}')
-        rucio_create_ds_proc = subprocess.run(cmd, shell=True)
-        assert rucio_create_ds_proc.returncode == 0
-        return dataset_did
-
-
-    def rucio_upload(self, f):
-        account_arg = '-a {rucio_account}'.format(rucio_account=self.rucio_account)
-        rse_arg = '--rse {start_rse}'.format(start_rse=self.start_rse)
-        cmd = 'rucio {account_arg} upload {rse_arg} {f}'\
-            .format(account_arg=account_arg, rse_arg=rse_arg, filepath=filepath)
-        logger.info(f'Running command: {cmd}')
-        rucio_upload_proc = subprocess.run(cmd, shell=True)
-        assert rucio_upload_proc.returncode == 0
-
-
-    def rucio_attach_dataset(self, dataset_did, didfile_path):
-        account_arg = '-a {rucio_account}'.format(rucio_account=self.rucio_account)
-        didfile_arg = '-f {didfile_path}'.format(didfile_path=didfile_path)
-        cmd = 'rucio {account_arg} attach {dataset_did} {didfile_arg}'\
-            .format(account_arg=account_arg, dataset_did=dataset_did, didfile_arg=didfile_arg)
-        logger.info(f'Running command: {cmd}')
-        rucio_attach_ds_proc = subprocess.run(cmd, shell=True)
-        assert rucio_attach_ds_proc.returncode == 0
+        logger.info(f'Creating Rucio dataset')
+        rc = self.rucio_client.add_dataset(
+                self.test_params.rucio_scope,
+                dataset_name,
+                rse=self.rse
+        )
+        return f'{self.scope}:{self.dataset_name}'
 
 
 def get_file_queues(num_threads, f):
@@ -71,14 +62,14 @@ def get_file_queues(num_threads, f):
 
 def main():
     args = get_program_arguments()
-    uploader = RucioUploader(args)
+    uploader = RucioUploader(args.rucio_account, args.scope, args.dataset_name, args.rse)
     uploader.rucio_create_dataset()
 
     threads = []
     with open(args.filelist) as f: # Obtain the work distribution and hand it to the threads
         file_queues =  get_file_queues(args.num_threads, f)
         for i in range(args.num_threads):
-            t = threading.Thread(target=uploader.do_processing, args=(i, file_queues[i], args))
+            t = threading.Thread(target=uploader.do_processing, args=(i, file_queues[i]))
             t.start()
             threads.append(t)
     for t in threads:
@@ -88,9 +79,11 @@ def main():
 def get_program_arguments():
     parser = argparse.ArgumentParser(description="Rucio Bulk Upload: Helper to upload files to Rucio in parallel trivially.")
     parser.add_argument('rucio-account', help='Rucio account to be used.')
-    parser.add_argument('dataset-did', help='Rucio formated DID (scope:name) of the dataset to be created that all uploaded files are to be attached to.')
+    parser.add_argument('dataset-name', help='Name of the dataset to be created that all uploaded files are to be attached to.')
+    parser.add_argument('rse', help='Rucio Storage Element that the files will be uploaded to.')
     parser.add_argument('filelist', help='Text file with one file name per line of the files to be uploaded.')
     parser.add_argument('--num-threads', type=int, default=1, help='Number of threads to divy up filelist between.')
+    parser.add_argument('--scope', help='Rucio scope that the files are to be placed in. Default: user.{rucio-account}')
 
     args = parser.parse_args()
     return args

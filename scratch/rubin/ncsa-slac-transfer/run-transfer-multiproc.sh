75993b88498ba503aa0b219e6430872aa116e2c9
Brandon White <bjwhite@fnal.gov>
2022-08-01 09:30:20 -0700

    Change transfer dest

--- scratch/rubin/ncsa-slac-transfer/run-transfer-multiproc.sh
+++ scratch/rubin/ncsa-slac-transfer/run-transfer-multiproc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xfer_dest_dir=/sdf/group/rubin/scratch
+xfer_dest_dir=/sdf/group/rubin/scratch/ncsa-tarballs
 pwd_f=/root/ncsapwd.txt
 host_list=gpfs12.ncsa.illinois.edu,gpfs13.ncsa.illinois.edu,gpfs14.ncsa.illinois.edu,gpfs15.ncsa.illinois.edu,gpfs16.ncsa.illinois.edu,gpfs17.ncsa.illinois.edu,gpfs18.ncsa.illinois.edu,gpfs19.ncsa.illinois.edu
 parallel_rsync_script=/sdf/home/b/bjwhite/spellbook/general/parallel-rsync.py

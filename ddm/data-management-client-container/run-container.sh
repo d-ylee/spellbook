16f54cedcd396850b3302a83541052c8915e7527
Brandon White <bjwhite@fnal.gov>
2023-08-28 11:12:48 -0600

    Makes me feel cool.

--- ddm/data-management-client-container/run-container.sh
+++ ddm/data-management-client-container/run-container.sh
@@ -5,8 +5,10 @@ cp /tmp/x509up_u$(id -u) ./x509up_u1000
 podman run \
     --mount 'type=bind,src=./x509up_u1000,dst=/tmp/x509up_u1000' \
     --mount 'type=bind,src=./rucio.cfg,dst=/opt/rucio/etc/rucio.cfg' \
+    --mount 'type=bind,src=./datadir,dst=/data' \
     --env 'X509_USER_PROXY=/tmp/x509up_u1000' \
-    --env 'PS1=\e[40;32mVIPER@GALACTICA :\w$ \e[32m' \
+    --env 'PS1=\e[40;32mVIPER@$CONTAINERHOST :\w$ \e[32m' \
+    --env "CONTAINERHOST=$HOSTNAME" \
     --name ddm-container \
     -it \
     --rm \
